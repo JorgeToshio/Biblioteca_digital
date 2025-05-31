e95b7339c053f626ecf1ea6e371b431fc1270d76
JorgeToshio <jtukaizensab@gmail.com>
2025-05-31 15:27:44 -0300

    Delete gestao-biblioteca directory

--- gestao-biblioteca/src/main/java/com/gestao/biblioteca/repository/EmprestimoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gestao.biblioteca.repository;
-
-import com.gestao.biblioteca.model.Emprestimo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmprestimoRepository extends JpaRepository<Emprestimo, Integer> {
-}
--- gestao-biblioteca/src/main/java/com/gestao/biblioteca/repository/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gestao.biblioteca.repository;
-
-import com.gestao.biblioteca.model.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivroRepository extends JpaRepository<Livro, Integer> {
-}
